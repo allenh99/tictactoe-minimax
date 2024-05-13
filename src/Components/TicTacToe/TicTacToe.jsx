5dccc4b2b78a4a2223556301535c413ce6c24168
Allen <allen@Kevins-MBP-4.fios-router.home>
2024-05-13 15:43:49 -0400

    commit 2

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -9,6 +9,7 @@ const TicTacToe = () => {
 
     let [count,setCount] = useState(0);
     let [lock,setLock] = useState(false); 
+    let titleRef = useRef(null);
     let box1 = useRef(null);
     let box2 = useRef(null);
     let box3 = useRef(null);
@@ -112,13 +113,20 @@ const TicTacToe = () => {
         }
         e.target.innerHTML = `<img src = '${cross_icon}'>`;
         data[num] = "x";
-    
-        let bot_move = findBest(data);
-        console.log(bot_move)
-        data[bot_move] = "o";
-        document.getElementById(String(bot_move)).innerHTML = `<img src = '${circle_icon}'>`;
-        console.log(data)
-        calculateWinner(data)
+        if (isMovesLeft(data) == false){
+            console.log("NO MOVES LEFT")
+            calculateWinner(data)
+            return null;
+        }
+        setTimeout(() => {
+            let bot_move = findBest(data);
+            console.log(bot_move)
+            data[bot_move] = "o";
+            document.getElementById(String(bot_move)).innerHTML = `<img src = '${circle_icon}'>`;
+            console.log(data)
+            calculateWinner(data)
+        }, 500)
+
     }
 
     function calculateWinner(squares) {
@@ -136,14 +144,30 @@ const TicTacToe = () => {
             const [a, b, c] = lines[i];
             if (squares[a] != "" && squares[a] === squares[b] && squares[a] === squares[c]) {
                 console.log("WINNER!")
-                return won(squares);
+                return won(squares[a]);
             }
+        }
+        if (isMovesLeft(data) == false){
+            return won("tie");
         }
         return null;
     }
 
     const won = (winner) => {
-        setLock(true);
+        //console.log(winner + " won!")
+        if (winner =="x"){
+            titleRef.current.innerHTML = 'You won!'
+            setLock(true);
+        }
+        else if(winner == "o"){
+            titleRef.current.innerHTML = "You lost!"
+            setLock(true);
+        }
+        else{
+            titleRef.current.innerHTML = "Tie!"
+            setLock(true);
+        }
+        
     }
 
     const reset = () => {
@@ -153,6 +177,7 @@ const TicTacToe = () => {
         box_array.map((e)=>{
             e.current.innerHTML = "";
         })
+        titleRef.current.innerHTML = "";
     }   
 
     return (
@@ -175,6 +200,7 @@ const TicTacToe = () => {
                 <div className="boxes" id = "8" ref = {box9} onClick={(e)=>{toggle(e,8)}}></div>
             </div>
         </div>
+        <h1 className="title" ref = {titleRef}></h1>
         <button className="reset" onClick={()=>{reset()}}>Reset</button>
     </div>
   )
